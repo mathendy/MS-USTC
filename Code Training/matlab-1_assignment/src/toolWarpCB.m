8cacd86d37f9f512e19796f63ef16214783b3c41
kanhaowei <khw1123@mail.ustc.edu.cn>
2020-09-17 18:13:18 +0800

    tic toc

--- Code Training/matlab-1_assignment/src/toolWarpCB.m
+++ Code Training/matlab-1_assignment/src/toolWarpCB.m
@@ -8,6 +8,7 @@ p2p = zeros(numel(hlines)*2,2);
 for i=1:numel(hlines)
     p2p(i*2+(-1:0),:) = hlines(i).getPosition();
 end
-
+tic
 im2 = IDWImageWarp(im, p2p(1:2:end,:), p2p(2:2:end,:));
+toc
 set(himg, 'CData', im2);
